53ea474cb6074c3fbafb71a52e828ad80ac9b5fe
BerlinDeveloper <marianoespinoza.cord@gmail.com>
2022-09-28 15:04:50 -0500

    Fin de la seccion 6

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,16 +9,31 @@ export class InfoPaginaService {
 
   info: InfoPagina = {};
   cargada = false;
+  equipo: any[] = [];
 
   constructor(private http: HttpClient) { 
-    // console.log("Servicio infopagina listo");
+   this.cargarInfo();
+   this.cargarEquipo();
+  }
+
+  private cargarInfo(){
     // leer archivo json y tomar sus propiedades en las demas paginas
     this.http.get('assets/data/data-pagina.json').subscribe(
       (resp:  InfoPagina)=> {
       //    console.log( resp['twitter']); investigar no me deja tomar la propiedad de este objeto
       this.info = resp;
       this.cargada = true;
-        console.log(resp); 
+       // console.log(resp); 
       });
   }
+
+  private cargarEquipo() {
+    this.http.get('https://test-angular-mec-default-rtdb.firebaseio.com/equipo.json').subscribe(
+      (resp:  any)=> {
+      this.equipo = resp;
+        // console.log(resp); se comenta solo se usa para test 
+      });
+  }
+
+
 }
